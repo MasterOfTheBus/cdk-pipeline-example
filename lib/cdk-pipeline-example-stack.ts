4a6d37840c887b0f9fb87b3a18c7e67cebf07ac3
Sidney Ng <yendisng@gmail.com>
2021-11-21 18:10:02 -0500

    adding a lambda stack and deploying as part of the pipeline

--- lib/cdk-pipeline-example-stack.ts
+++ lib/cdk-pipeline-example-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
+// import { ManualApprovalStep } from '@aws-cdk/pipelines';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
 export class CdkPipelineExampleStack extends cdk.Stack {
@@ -14,6 +16,27 @@ export class CdkPipelineExampleStack extends cdk.Stack {
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "025257542471", region: "us-east-1"}
+    }));
+
+    /** Defines a testing stage with manual approval as a post-deployment step */
+    // const testingStage = pipeline.addStage(new MyPipelineAppStage(this, 'testing', {
+    //   env: { account: '025257542471', region: 'us-east-1' }
+    // }));
+    //
+    // testingStage.addPost(new ManualApprovalStep('approval'));
+
+    /** Defines a wave of parallel deployment stages */
+    // const wave = pipeline.addWave('wave');
+    // wave.addStage(new MyApplicationStage(this, 'MyAppEU', {
+    //   env: { account: '025257542471', region: 'us-east-1' }
+    // }));
+    // wave.addStage(new MyApplicationStage(this, 'MyAppUS', {
+    //   env: { account: '025257542471', region: 'us-west-1' }
+    // }));
+
 
     // example resource
     // const queue = new sqs.Queue(this, 'CdkPipelineExampleQueue', {
