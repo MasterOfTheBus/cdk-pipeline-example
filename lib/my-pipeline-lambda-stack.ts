5a5b99c8dd4824394e8831a73db20f5318841668
Sidney Ng <yendisng@gmail.com>
2021-11-28 15:35:41 -0500

    define the lambda name

--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -15,6 +15,7 @@ export class MyLambdaStack extends cdk.Stack {
 
       if (props?.bucket && props?.s3Artifact) {
         new Function(this, 'LambdaFunction', {
+          functionName: 'TestCdkLambda',
           runtime: Runtime.PYTHON_3_9,
           handler: 'sample_lambda_handler.handler',
           code: Code.fromBucket(props.bucket, props.s3Artifact.objectKey)
