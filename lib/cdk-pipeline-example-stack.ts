40f24f2629feae570f2969ddd014a9174ed7727e
Sidney Ng <yendisng@gmail.com>
2021-12-23 15:44:34 -0500

    Revert "using customer package for pipeline"
    This reverts commit 39eb4bbf9c0c56f432435af27a54eb7868384f16.

--- lib/cdk-pipeline-example-stack.ts
+++ lib/cdk-pipeline-example-stack.ts
@@ -1,27 +1,23 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { CodeStarConnectionDef, MultiSourcePipeline } from '@masterofthebus/cdk-pipeline-lib/lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class CdkPipelineExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const source = new CodeStarConnectionDef({
-      // A CodeStar Connection ARN
-      codeStarConnection: "arn:aws:codestar-connections:us-east-1:000000000000:connection/11111111-2222-3333-4444-555555555555",
-      repo: "cdk-pipeline-example",
-      repoOwner: "MasterOfTheBus",
-      branch: "main"
+    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CdkPipelineExample',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('MasterOfTheBus/cdk-pipeline-example', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
     });
 
-    // Define the bucket to store the artifacts
-    const bucket = new Bucket(this, 'PipelineBucket');
-
-    // The pipeline construct
-    new MultiSourcePipeline(this, 'MultiSourcePipline', {
-        sources: [source],
-        deployBucket: bucket
-    });
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "025257542471", region: "us-east-1"}
+    }));
   }
 }
