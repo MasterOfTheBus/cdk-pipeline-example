2c22328b215fc279469ee57e3094476f040a6355
Sidney Ng <yendisng@gmail.com>
2021-12-23 15:45:29 -0500

    Revert custom pkg (#3)
    * Revert "setting the code star connection ARN"
    
    This reverts commit e4c06e3338fd8cbafa5e98d14a838547ff8261b0.
    
    * Revert "using customer package for pipeline"
    
    This reverts commit 39eb4bbf9c0c56f432435af27a54eb7868384f16.

--- lib/cdk-pipeline-example-stack.ts
+++ lib/cdk-pipeline-example-stack.ts
@@ -1,27 +1,23 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { CodeStarConnectionDef, MultiSourcePipeline } from '@masterofthebus/cdk-pipeline-lib/lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class CdkPipelineExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const source = new CodeStarConnectionDef({
-      // A CodeStar Connection ARN
-      codeStarConnection: "arn:aws:codestar-connections:us-east-1:025257542471:connection/b53232ef-36cd-40e2-90ce-4bed059aed57",
-      repo: "cdk-pipeline-example",
-      repoOwner: "MasterOfTheBus",
-      branch: "main"
+    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CdkPipelineExample',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('MasterOfTheBus/cdk-pipeline-example', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
     });
 
-    // Define the bucket to store the artifacts
-    const bucket = new Bucket(this, 'PipelineBucket');
-
-    // The pipeline construct
-    new MultiSourcePipeline(this, 'MultiSourcePipline', {
-        sources: [source],
-        deployBucket: bucket
-    });
+    pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "025257542471", region: "us-east-1"}
+    }));
   }
 }
