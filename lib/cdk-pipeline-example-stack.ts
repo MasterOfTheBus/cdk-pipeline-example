eb245ab85cff63d191172f8a5c5ceef02dd08932
Sidney Ng <yendisng@gmail.com>
2021-12-29 20:52:30 -0500

    Feature/use construct (#4)
    * Revert "Revert custom pkg (#3)"
    
    This reverts commit 2c22328b215fc279469ee57e3094476f040a6355.
    
    * Use the custom package for code pipeline helpers
    
    * update the import

--- lib/cdk-pipeline-example-stack.ts
+++ lib/cdk-pipeline-example-stack.ts
@@ -1,23 +1,40 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { CodePipelineConstruct, CodeStarConnectionDef } from '@masterofthebus/cdk-pipeline-lib';
 import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class CdkPipelineExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'CdkPipelineExample',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('MasterOfTheBus/cdk-pipeline-example', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
+    const pipelineSource = new CodeStarConnectionDef({
+      // A CodeStar Connection ARN
+      codeStarConnection: "arn:aws:codestar-connections:us-east-1:025257542471:connection/b53232ef-36cd-40e2-90ce-4bed059aed57",
+      repo: "cdk-pipeline-example",
+      repoOwner: "MasterOfTheBus",
+      branch: "main"
     });
 
-    pipeline.addStage(new MyPipelineAppStage(this, "test", {
-      env: { account: "025257542471", region: "us-east-1"}
-    }));
+    const codeSource = new CodeStarConnectionDef({
+      // A CodeStar Connection ARN
+      codeStarConnection: "arn:aws:codestar-connections:us-east-1:025257542471:connection/b53232ef-36cd-40e2-90ce-4bed059aed57",
+      repo: "test_lambda_deploy",
+      repoOwner: "MasterOfTheBus",
+      branch: "main"
+    });
+
+    const bucketArn = 'arn:aws:s3:::sng-lambda-deployments-bucket';
+
+    const pipeline = new CodePipelineConstruct(this, 'CodePipeline', {
+      pipelineSource: pipelineSource,
+      source: codeSource,
+      artifactBucketArn: bucketArn
+    });
+
+    pipeline.pipeline.addStage(
+      new MyPipelineAppStage(this ,'TestLambda', bucketArn, `${codeSource.repo}/artifacts.zip`, {
+        env: { account: '025257542471', region: 'us-east-1' }
+      })
+    )
   }
 }
