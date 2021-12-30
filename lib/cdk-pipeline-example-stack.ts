5d6e5bd79b22e45ae553377822d533e6c504f018
Sidney Ng <yendisng@gmail.com>
2021-12-29 20:43:20 -0500

    Use the custom package for code pipeline helpers

--- lib/cdk-pipeline-example-stack.ts
+++ lib/cdk-pipeline-example-stack.ts
@@ -1,13 +1,13 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
-import { CodeStarConnectionDef, MultiSourcePipeline } from '@masterofthebus/cdk-pipeline-lib/lib';
+import { CodePipelineConstruct, CodeStarConnectionDef } from '@masterofthebus/cdk-pipeline-lib/lib';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class CdkPipelineExampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const source = new CodeStarConnectionDef({
+    const pipelineSource = new CodeStarConnectionDef({
       // A CodeStar Connection ARN
       codeStarConnection: "arn:aws:codestar-connections:us-east-1:025257542471:connection/b53232ef-36cd-40e2-90ce-4bed059aed57",
       repo: "cdk-pipeline-example",
@@ -15,13 +15,26 @@ export class CdkPipelineExampleStack extends Stack {
       branch: "main"
     });
 
-    // Define the bucket to store the artifacts
-    const bucket = new Bucket(this, 'PipelineBucket');
-
-    // The pipeline construct
-    new MultiSourcePipeline(this, 'MultiSourcePipline', {
-        sources: [source],
-        deployBucket: bucket
+    const codeSource = new CodeStarConnectionDef({
+      // A CodeStar Connection ARN
+      codeStarConnection: "arn:aws:codestar-connections:us-east-1:025257542471:connection/b53232ef-36cd-40e2-90ce-4bed059aed57",
+      repo: "test_lambda_deploy",
+      repoOwner: "MasterOfTheBus",
+      branch: "main"
     });
+
+    const bucketArn = 'arn:aws:s3:::sng-lambda-deployments-bucket';
+
+    const pipeline = new CodePipelineConstruct(this, 'CodePipeline', {
+      pipelineSource: pipelineSource,
+      source: codeSource,
+      artifactBucketArn: bucketArn
+    });
+
+    pipeline.pipeline.addStage(
+      new MyPipelineAppStage(this ,'TestLambda', bucketArn, `${codeSource.repo}/artifacts.zip`, {
+        env: { account: '025257542471', region: 'us-east-1' }
+      })
+    )
   }
 }
--- lib/my-pipeline-lambda-stack.ts
+++ lib/my-pipeline-lambda-stack.ts
@@ -1,15 +1,18 @@
 import { Construct } from 'constructs';
 import * as cdk from 'aws-cdk-lib/core';
-import { Function, InlineCode, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Function, Runtime, S3Code } from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class MyLambdaStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, bucketArn: string, key: string, props?: cdk.StackProps) {
       super(scope, id, props);
+
+      const bucket = Bucket.fromBucketArn(this, 'ArtifactBucket', bucketArn);
 
       new Function(this, 'LambdaFunction', {
         runtime: Runtime.NODEJS_12_X,
         handler: 'index.handler',
-        code: new InlineCode('exports.handler = _ => "Hello, CDK";')
+        code: new S3Code(bucket, key)
       });
     }
 }
